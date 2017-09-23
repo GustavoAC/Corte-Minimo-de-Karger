05a2ce429881962d6493d30dd4ff17c69a62bad3
Gustavo Carvalho <guga.a.carvalho@gmail.com>
2017-09-23 17:11:43 -0300

    working version

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <list>
 #include <utility>
 #include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <set>
 
 //	1)  Initialize contracted graph CG as copy of original graph
 //	2)  While there are more than 2 vertices.
@@ -13,25 +16,26 @@
 
 struct Edge {
 	int multiplicidade;
-	int std::vector<std::pair<int, int>> EdgeIds;
+	std::set<std::pair<int, int>> EdgeIds;
 	int destinyVertex;
 	std::list<Edge>::iterator reversePointer;
 };
 
 void preencherAdjList(const std::string &filename, std::vector<std::list<Edge>> &adjList, int &numArestas) {
 	int numVer;
-	std::ifstream file (filename);
+	std::ifstream file(filename);
 	file >> numVer >> numArestas;
 	adjList.resize(numVer);
 
-	int orig, dest;
-	Edge e1, e2;
-	e1.multiplicidade = e2.multiplicidade = 1;
+	int orig, dest;	
 
 	for (int i = 0; i < numArestas; ++i) {
 		file >> orig >> dest;
-		e1.EdgeIds.push_back(std::pair<int,int>(orig, dest));
-		e2.EdgeIds.push_back(std::pair<int,int>(orig, dest));
+		Edge e1, e2;
+		e1.multiplicidade = e2.multiplicidade = 1;
+		
+		e1.EdgeIds.insert(std::pair<int,int>(orig, dest));
+		e2.EdgeIds.insert(std::pair<int,int>(orig, dest));
 
 		e1.destinyVertex = dest;
 		e2.destinyVertex = orig;
@@ -46,14 +50,30 @@ void preencherAdjList(const std::string &filename, std::vector<std::list<Edge>> 
 	file.close();
 }
 
-void merge(std::vector<std::list<Edge>> &listaCopia, const int &originVertex, const int &destinyVertex, int &numArestas) {
+// void printListaCopia(std::vector<std::list<Edge>> &listaCopia) {
+// 	for (int i = 0; i < listaCopia.size(); ++i) {
+// 		std::cout << i << ":";
+// 		for (auto e : listaCopia[i]) {
+// 			std::cout << "(" << e.destinyVertex << "," << e.multiplicidade << ")";
+// 		}
+// 		std::cout << "\n";
+// 	}
+// }
+
+void merge(std::vector<std::list<Edge>> &listaCopia, const int &originVertex, const int destinyVertex, int &numArestas) {
 	for (auto edge = listaCopia[destinyVertex].begin(); edge != listaCopia[destinyVertex].end(); edge++) {
-		// if edge[i] for para origin, delete this
+			// std::string ke;
+			// std::cout << originVertex << " " << destinyVertex << " " << edge->destinyVertex << "\n";
+			// printListaCopia(listaCopia);
+			// std::cin >> ke;
+			// if edge[i] for para origin, delete this
 		if (edge->destinyVertex == originVertex) {
 			numArestas -= edge->multiplicidade;
 			
 			auto reverseIt = edge->reversePointer;
 			auto itselfIt = reverseIt->reversePointer;
+
+			edge--;
 
 			listaCopia[originVertex].erase(reverseIt);
 			listaCopia[destinyVertex].erase(itselfIt);
@@ -62,22 +82,40 @@ void merge(std::vector<std::list<Edge>> &listaCopia, const int &originVertex, co
 		}
 
 		// Verifica se a aresta está no vertice original
-		std::list<Edge>::iterator found = nullptr;
+		std::list<Edge>::iterator foundIt;
+		bool found = false;
 		for (auto e = listaCopia[originVertex].begin(); e != listaCopia[originVertex].end(); e++)
 			if (e->destinyVertex == edge->destinyVertex) {
-				found = e;
+				foundIt = e;
+				found = true;
 				break;
 			}
 		
 		// if edge[i] já tiver lá, aumenta a multiplicidade
-		if (found != nullptr) {
-			found->multiplicidade += edge->multiplicidade;
-			found->EdgeIds.insert(found->EdgeIds.end(), edge->EdgeIds.begin(), edge->EdgeIds.end());
+		// x = origin
+		// y = destiny
+		// z = edge->destinyVertex
+		// x->z
+		// y->z = edge
+		// x->z += y->z
+		std::list<Edge>::iterator zToY = edge->reversePointer;
+		if (found) {
+			foundIt->multiplicidade += edge->multiplicidade;
+			foundIt->EdgeIds.insert(edge->EdgeIds.begin(), edge->EdgeIds.end());
+			
+			int z = edge->destinyVertex;
+			std::list<Edge>::iterator zToX, zToY = edge->reversePointer;
+			for (zToX = listaCopia[z].begin(); zToX != listaCopia[z].end(); zToX++)
+				if (zToX->destinyVertex == originVertex) break;
+
+			zToX->multiplicidade += zToY->multiplicidade;
+			zToX->EdgeIds.insert(zToY->EdgeIds.begin(), zToY->EdgeIds.end());
+			listaCopia[z].erase(zToY);
 		} else {
-		// senao copia edge[i] para originVertex
-			Edge &destiny = *edge.reversePointer;
+			// senao copia edge[i] para originVertex
 			auto newEdgeIt = listaCopia[originVertex].insert(listaCopia[originVertex].end(), *edge);
-			destiny.reversePointer = newEdgeIt;
+			zToY->reversePointer = newEdgeIt;
+			zToY->destinyVertex = originVertex;
 		}
 	}
 
@@ -86,14 +124,18 @@ void merge(std::vector<std::list<Edge>> &listaCopia, const int &originVertex, co
 
 int main(int argc, char const *argv[]) {
 	std::vector<std::list<Edge>> listaCopia;
-	srand(time());
+	srand(time(NULL));
 
 	int numArestas;
-	
-	preencherAdjList(listaCopia, numArestas);
+	std::string filename;
+
+	std::cout << "Insira o nome do arquivo com as arestas do grafo:\n";
+	std::cin >> filename;
+
+	preencherAdjList(filename, listaCopia, numArestas);
 
 	// listaCopia = adjList;
-	for (int i = 0; i < adjList.size() - 2; i++) {
+	for (int i = 0; i < listaCopia.size() - 2; i++) {
 		int selectedEdge = (rand() % numArestas) + 1;
 		int origVer = -1;
 
@@ -111,11 +153,13 @@ int main(int argc, char const *argv[]) {
 		merge(listaCopia, origVer, it->destinyVertex, numArestas);
 	}
 
+	printListaCopia(listaCopia);
+
 	// percorre até encontrar a última aresta
 	// corte mínimo será EdgeIds
 	// tamanho do corte mínimo será EdgeIds.size()
 	std::list<Edge>::iterator resEdge;
-	std::vector<std::pair<int, int>> res;
+	std::set<std::pair<int, int>> res;
 	for (int i = 0; i < listaCopia.size(); i++)
 		if (!listaCopia[i].empty()) {
 			res = listaCopia[i].begin()->EdgeIds;
@@ -124,7 +168,7 @@ int main(int argc, char const *argv[]) {
 
 	std::cout << "Tamanho do corte mínimo: " << res.size() << "\n";
 	std::cout << "O corte mínimo é formado pelas arestas: ";
-	for (int i = 0; i < res.size(); ++i) std::cout << "(" << res[i].first << ", " << res[i].second << ") ";
+	for (auto pair : res) std::cout << "(" << pair.first << ", " << pair.second << ") ";
 	std::cout << "\n";
 	
 	return 0;
